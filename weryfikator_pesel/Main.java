7f6d1498cf2655bb022dcb6d3022b86a79608a17
mawojcik <wojcik.maciej.k@gmail.com>
2022-10-17 19:30:42 +0200

    Final version

---
+++ weryfikator_pesel/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        if (args[0].length() != 11) {
+            System.out.println("Run with correct pesel number!");
+            System.exit(0);
+        }
+        PESEL pesel_input = new PESEL(args[0]);
+        PESEL pesel_to_compare = new PESEL("78040241369");
+
+        PESEL.check(args[0]);
+        PESEL.compare(pesel_to_compare, pesel_input);
+
+        System.out.println("\nPesel input:      " + pesel_input.pesel);
+        System.out.println("Pesel to compare: " + pesel_to_compare.pesel);
+    }
+}
+
+
