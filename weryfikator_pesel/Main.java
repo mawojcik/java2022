d83ffd032ffabe4f90745795487de851231af228
mawojcik <wojcik.maciej.k@gmail.com>
2022-10-27 12:04:02 +0200

    Files to work with

--- weryfikator_pesel/Main.java
+++ weryfikator_pesel/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        if (args[0].length() != 11) {
+        if (args.length <1 || args[0].length() != 11) {
             System.out.println("Run with correct pesel number!");
             System.exit(0);
         }
@@ -10,8 +10,8 @@ public class Main {
         PESEL.check(args[0]);
         PESEL.compare(pesel_to_compare, pesel_input);
 
-        System.out.println("\nPesel input:      " + pesel_input.pesel);
-        System.out.println("Pesel to compare: " + pesel_to_compare.pesel);
+        System.out.println("\nPesel input:      " + pesel_input.getPesel());
+        System.out.println("Pesel to compare: " + pesel_to_compare.getPesel());
     }
 }
 
